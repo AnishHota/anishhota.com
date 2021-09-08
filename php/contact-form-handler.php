c0bd4ae3c6d905f7b8a95afdb094afd5c2ad0edb
anishhota <anishhota92@gmail.com>
2021-09-08 09:39:39 +0530

    Contact page under construction

---
+++ php/contact-form-handler.php
@@ -0,0 +1,54 @@
+ 
+   <?PHP
+
+        echo "Inside the file";
+        // error_reporting(E_ALL);
+        // ini_set('display_errors', 1);
+
+        // $email_from="feedback@awesome.com"; 
+        // // $pcount=0;
+        // // $gcount=0;
+        // $email_subject = "Mail from Feedback Form";
+
+        // $email_to="anishhota1@gmail.com";
+
+        // $name = $_POST['name'];
+        // $visitor_email = $_POST['email'];
+        // $message = $_POST['message'];
+
+        // $email_body = "User name: $name. \n"."User Email: $email. \n"."User message: $message. \n";
+
+        // $headers = "From: $email_from \r\n";
+
+        // mail($to,$email_subject,$email_body,$headers)
+
+        // header("Location: contact.html")
+
+
+
+
+        // while (list($key,$val)=each($_POST))
+        // {
+        // $pstr = $pstr."$key : $val \n ";
+        // ++$pcount;
+
+        // }
+        // while (list($key,$val)=each($_GET))
+        // {
+        // $gstr = $gstr."$key : $val \n ";
+        // ++$gcount;
+
+        // }
+        // if ($pcount > $gcount)
+        // {
+        // $message_body=$pstr;
+        // mail($mailto,$subject,$message_body,"From:".$from);
+        // echo "Mail has been sent";
+        // }
+        // else
+        // {
+        // $message_body=$gstr;
+        // mail($mailto,$subject,$message_body,"From:".$from);
+        // echo "Mail has been sent";
+        // }
+?>
