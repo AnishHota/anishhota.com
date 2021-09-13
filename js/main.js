4933c3a7093ff001519ac0ca7f8f650a98291413
anishhota <anishhota92@gmail.com>
2021-09-13 23:22:31 +0530

    Navbar responsive

--- js/main.js
+++ js/main.js
@@ -1,22 +1,42 @@
 $(document).ready(function(){
 
 // Sticky navigation menu
-let nav_offset_top =$('.header_area').height() + 50;
+// let nav_offset_top =$('.header_area').height() + 2;
 
-function navbarFixed(){
-    if($('.header_area').length){
-        $(window).scroll(function(){
-            let scroll = $(window).scrollTop();
-            if(scroll>=nav_offset_top){
-                $('.header_area .main-menu').addClass('navbar_fixed');
-            }else{
-                $('.header_area .main-menu').removeClass('navbar_fixed');
-            }
-        })
-    }
+// let nav_offset_top =$('.header_area').height();
+
+// function navbarFixed(){
+//     if($('.header_area').length){
+//         $(window).scroll(function(){
+//             let scroll = $(window).scrollTop();
+//             if(scroll>=nav_offset_top){
+//                 $('.header_area .main-menu').addClass('navbar_fixed');
+//             }else{
+//                 $('.header_area .main-menu').removeClass('navbar_fixed');
+//             }
+//         })
+//     }
+// }
+// navbarFixed();
+
+// TODO: Navbar_fixed for Responsive: global.css
+
+const nav = document.querySelector('.header_area');
+let navTop = nav.offsetTop;
+
+console.log(navTop);
+
+function fixedNav() {
+  if (window.scrollY >= navTop + 50) {    
+    nav.classList.add('navbar_fixed');
+  } else {
+    nav.classList.remove('navbar_fixed');    
+  }
 }
 
-navbarFixed();
+window.addEventListener('scroll', fixedNav);
+
+
 
 // Typing animation script
 var typed = new Typed(".typing", {
